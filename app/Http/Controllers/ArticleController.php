83b57e48d8826c5169d5436dc672cc4115c71cc0
Hani Gerges <hani@localhost.localdomain>
2021-01-05 16:21:37 +0200

    image required

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -22,9 +22,9 @@ class ArticleController extends Controller
             {
                 return response()->json(['status'=>'No Category Found with this name'],404);
             }
-            $articles=Article::where('category_id',$category->id)->paginate(9);
+            $articles=Article::where('category_id',$category->id)->paginate(12);
         }
-        else $articles=Article::paginate(9);
+        else $articles=Article::paginate(12);
 
         return ArticleResource::collection($articles);
     }
