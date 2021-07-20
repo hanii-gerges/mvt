9b024e65cd5fc48baccb24f8b84a9a55ac0d4ee1
hanii-gerges <hanigerges05@gmail.com>
2021-07-20 16:28:19 +0300

    added slider/about/social_media tables to migration

---
+++ app/Http/Controllers/HeroSliderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HeroSlider;
+use Illuminate\Http\Request;
+
+class HeroSliderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HeroSlider  $heroSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HeroSlider $heroSlider)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HeroSlider  $heroSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HeroSlider $heroSlider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HeroSlider  $heroSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HeroSlider $heroSlider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HeroSlider  $heroSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HeroSlider $heroSlider)
+    {
+        //
+    }
+}
